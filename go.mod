83961401ea26c07cc160c1b9295c51bd6935c33b
blackbracken <me@bracken.black>
2020-02-28 17:47:32 +0900

    load configurations from exec dir

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/sparrc/go-ping v0.0.0-20190613174326-4e5b6552494c
 	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
+	gopkg.in/yaml.v2 v2.2.8
 )
--- main.go
+++ main.go
@@ -1,14 +1,61 @@
 package main
 
 import (
+	"fmt"
 	"github.com/sparrc/go-ping"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 )
 
+type Config struct {
+	Slack struct {
+		WebHookURL string
+		Mention    string
+	}
+	Pinged []string
+}
+
+type anyMap = map[interface{}]interface{}
+
 func main() {
-	pinger, err := ping.NewPinger("www.google.com")
+	exe, err := os.Executable()
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to get this executable")
 	}
 
+	fmt.Println(filepath.Dir(exe))
+	buf, err := ioutil.ReadFile(filepath.Dir(exe) + "/config.yml")
+	if err != nil {
+		log.Fatal("Failed to get config.yml")
+	}
+
+	m := make(anyMap)
+	err = yaml.Unmarshal(buf, &m)
+	if err != nil {
+		log.Fatal("Failed to parse config.yml")
+	}
+
+	slack := m["Slack"].(anyMap)
+	webhook := slack["WebHookURL"].(string)
+	mention := slack["Mention"].(string)
+
+	fmt.Println(webhook)
+	fmt.Println(mention)
+
+	p := m["Pinged"].([]interface{})
+	for i := range p {
+		s := p[i].(string)
+		fmt.Println(s)
+	}
+
+	pinger, err := ping.NewPinger("google.com")
+	if err != nil {
+		log.Fatal("Failed to send a ping google.com")
+	}
+	pinger.SetPrivileged(true)
+	pinger.Count = 4
 	pinger.Run()
 }
