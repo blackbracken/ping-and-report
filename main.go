fdb5651bd03cc21c112ca64f6b44976993fde86f
blackbracken <me@bracken.black>
2020-02-28 21:56:00 +0900

    log availables

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/sparrc/go-ping"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
@@ -20,6 +22,19 @@ type Config struct {
 	Pinged []string
 }
 
+type Available struct {
+	AddressAvailables map[string]AddressAvailable
+}
+
+type AddressAvailable struct {
+	CountTrying   uint64
+	CountSucceed  uint64
+	LastAvailable bool
+}
+
+const CONFIG = "config.yml"
+const AVAILABLE = "available.json"
+
 func main() {
 	exe, err := os.Executable()
 	if err != nil {
@@ -27,15 +42,36 @@ func main() {
 	}
 	dir := filepath.Dir(exe)
 
-	buf, err := ioutil.ReadFile(dir + "/config.yml")
-	if err != nil {
-		log.Fatal("Failed to get config.yml")
+	var cfg Config
+	cfgp := dir + "/" + CONFIG
+	{
+		cfg = Config{}
+
+		buf, err := ioutil.ReadFile(cfgp)
+		if err != nil {
+			log.Fatal("Failed to read " + cfgp)
+		}
+		err = yaml.Unmarshal(buf, &cfg)
+		if err != nil {
+			log.Fatal("Failed to parse " + cfgp)
+		}
 	}
 
-	cfg := Config{}
-	err = yaml.Unmarshal(buf, &cfg)
-	if err != nil {
-		log.Fatal("Failed to parse config.yml")
+	var avb Available
+	avbp := dir + "/" + AVAILABLE
+	{
+		avb = Available{map[string]AddressAvailable{}}
+
+		if exists(avbp) {
+			buf, err := ioutil.ReadFile(avbp)
+			if err != nil {
+				log.Fatal("Failed to read " + avbp)
+			}
+			err = json.Unmarshal(buf, &avb)
+			if err != nil {
+				log.Fatal("Failed to parse " + avbp)
+			}
+		}
 	}
 
 	for _, addr := range cfg.Pinged {
@@ -46,20 +82,44 @@ func main() {
 
 		pinger.Count = 3
 		pinger.OnFinish = func(s *ping.Statistics) {
+			addravb, ok := avb.AddressAvailables[s.Addr]
+			if !ok {
+				addravb = AddressAvailable{0, 0, true}
+			}
+
+			suc := s.PacketsRecv > 0
+
+			addravb.CountTrying++
+			addravb.LastAvailable = suc
+
 			var msg string
-			if s.PacketsRecv == 0 {
-				msg = "DOWN: " + s.Addr
-			} else {
+			if suc {
+				addravb.CountSucceed++
 				msg = "UP  : " + s.Addr
+			} else {
+				msg = "DOWN: " + s.Addr
 			}
 
 			err := report(cfg.Slack.WebHookURL, cfg.Slack.Mention, msg)
 			if err != nil {
 				log.Fatal("Failed to report")
 			}
+
+			avb.AddressAvailables[s.Addr] = addravb
 		}
 		pinger.Timeout = 10 * time.Second
 		pinger.Run()
+	}
+
+	fmt.Println(avb)
+
+	jsonBytes, err := json.Marshal(avb)
+	if err != nil {
+		log.Fatal("Failed to parse struct")
+	}
+	err = ioutil.WriteFile(avbp, jsonBytes, 0666)
+	if err != nil {
+		log.Fatal("Failed to write json")
 	}
 }
 
@@ -75,4 +135,9 @@ func report(url string, mention string, text string) error {
 	}
 
 	return nil
+}
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
 }
